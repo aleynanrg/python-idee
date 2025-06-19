2746ac97d70360dbc81b6d42ab461791031badde
Aleyna <aleynanuroglu@gmail.com>
2025-06-19 23:42:25 +0300

    ide

--- main.js
+++ main.js
@@ -1,39 +1,14 @@
 
 let pyodideReadyPromise = loadPyodide();
-let editor;
-
-window.onload = () => {
-  editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-    mode: "python",
-    theme: "default",
-    lineNumbers: true,
-    indentUnit: 4,
-    viewportMargin: Infinity
-  });
-};
 
 async function runCode() {
   const output = document.getElementById("output");
-  const inputVal = document.getElementById("inputBox").value;
-  output.textContent = "⏳ Kod çalıştırılıyor...";
+  output.textContent = "Çalıştırılıyor...";
   let pyodide = await pyodideReadyPromise;
-
-  let code = editor.getValue().replace("input(\"Adınızı girin:\")", `"${inputVal}"`);
-
   try {
-    let result = await pyodide.runPythonAsync(code);
+    let result = await pyodide.runPythonAsync(document.getElementById("code").value);
     output.textContent = result ?? "(Çıktı yok)";
   } catch (err) {
-    output.textContent = "🚫 Hata:\n" + err;
+    output.textContent = "Hata:\n" + err;
   }
-}
-
-function clearOutput() {
-  document.getElementById("output").textContent = "";
-}
-
-function toggleTheme() {
-  const body = document.body;
-  body.classList.toggle("dark");
-  body.classList.toggle("light");
 }
